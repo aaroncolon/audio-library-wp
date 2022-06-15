e2408424662cd53ba87b4c35fa7a006ce062969a
Aaron Colón <aaron.colon@gmail.com>
2022-06-14 20:02:33 -0500

    add AWS S3 presigned requests

---
+++ inc/aws-s3.php
@@ -0,0 +1,32 @@
+<?php
+require get_stylesheet_directory() .'/inc/aws-sdk-3/aws.phar';
+
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+
+function ml_get_s3_presigned_request($filename = '') {
+  if (! defined('AS3CF_SETTINGS')) { return null; }
+
+  $awsCreds = unserialize(AS3CF_SETTINGS);
+  $awsKey = $awsCreds['access-key-id'];
+  $awsSecret = $awsCreds['secret-access-key'];
+
+  $s3Client = new Aws\S3\S3Client([
+    // 'profile' => 'default',
+    'region' => 'us-east-1',
+    'version' => 'latest',
+    'credentials' => [
+      'key'    => $awsKey,
+      'secret' => $awsSecret,
+    ]
+  ]);
+  
+  $cmd = $s3Client->getCommand('GetObject', [
+    'Bucket' => 'livedogdeadlion',
+    'Key' => 'Website__Inspired_Eyes-Slashedover_-SCR50341.mp3'
+  ]);
+  
+  $request = $s3Client->createPresignedRequest($cmd, '+5 minutes');
+
+  return $presignedUrl = (string) $request->getUri();
+}
